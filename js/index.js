8afb1aee7dcd2ff9642f00f5751a5c63637c1c79
Patrick Kim <pkim050@gmail.com>
2019-06-10 15:16:57 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function displayCommits() {
   var commits = JSON.parse(this.responseText);
   const commList = `<ul>${commits.map(r =>
     `<li>${r.html_url}</li>`).join('')}</ul>`;
-  console.log(commList);
+  console.log(commits);
   document.getElementById('details').innerHTML = commList;
 }
 
