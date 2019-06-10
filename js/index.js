b247ee42f95358e1b30f804a459bea77da87b719
Patrick Kim <pkim050@gmail.com>
2019-06-10 14:39:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // your code here
+function displayCommits() {
+
+}
+
+function displayBranches() {
+
+}
+
+function displayRepositories() {
+  var repos = JSON.parse(this.responseText);
+  const repoList = `${repos.map(r =>
+    '<li></li>').join(' ')}`
+}
+
+function getRepositories() {
+  const username = document.getElementById("username").value;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
+
+function getCommits() {
+
+}
+
+function getBranches() {
+
+}
