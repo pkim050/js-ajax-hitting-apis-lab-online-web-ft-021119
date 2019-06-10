b40144cf8e064bba0b86e064a63782bbeabd6d2a
Patrick Kim <pkim050@gmail.com>
2019-06-10 14:53:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -30,6 +30,11 @@ function getCommits(el) {
   req.send();
 }
 
-function getBranches() {
-
+function getBranches(el) {
+  const username = el.dataset.username;
+  const repos = el.dataset.repository;
+  const req = new XMLHttpRequest;
+  req.addEventListener('load', displayBranches);
+  req.open('GET', `https://api.github.com/repos/${username}/${repos}/branches`);
+  req.send();
 }
